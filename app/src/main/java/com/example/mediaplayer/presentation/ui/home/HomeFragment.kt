97fadd0531df49a2c79d846e195c5ba5d7339819
Armen <armenbeglaryan993@gmail.com>
2022-05-24 17:16:02 +0400

    changed exoplayer

--- app/src/main/java/com/example/mediaplayer/presentation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mediaplayer/presentation/ui/home/HomeFragment.kt
@@ -1,18 +1,21 @@
 package com.example.mediaplayer.presentation.ui.home
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.fragment.app.Fragment
 import androidx.media3.common.MediaItem
 import androidx.media3.common.util.Util
-import androidx.media3.datasource.DefaultDataSource
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
 import com.example.mediaplayer.R
@@ -22,13 +25,10 @@ import com.example.mediaplayer.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
-
-
     private var player: ExoPlayer? = null
     private var playWhenReady = true
     private var currentItem = 0
     private var playbackPosition = 0L
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,40 +36,31 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putLong("position",playbackPosition)
+    }
 
-
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+       if (savedInstanceState?.get("position")!=null) {
+           playbackPosition = savedInstanceState.get("position") as Long
+       }
     }
 
     private fun initializePlayer() {
-        val btnMute = view?.findViewById<AppCompatImageView>(R.id.volume_control)
-        val btnPlay = view?.findViewById<AppCompatImageView>(R.id.btn_exo_play)
-        val btnNext = view?.findViewById<AppCompatImageView>(R.id.btn_exo_next)
-        val btnPrevious = view?.findViewById<AppCompatImageView>(R.id.btn_exo_previous)
-        val btnForward = view?.findViewById<AppCompatImageView>(R.id.btn_exo_fast_forward)
-        val btnRewind = view?.findViewById<AppCompatImageView>(R.id.btn_exo_fast_rewind)
         var volumeInfo = true
-        var play = true
+        var fullScreen = true
         var currentPosition = 0L
+       val matchParent =  ConstraintLayout.LayoutParams.MATCH_PARENT
 
-//        val dataSourceFactory: DataSource.Factory = DefaultHttpDataSource.Factory()
-//
-//        val mediaSourceFactory: MediaSource.Factory =
-//            DefaultMediaSourceFactory(dataSourceFactory)
-//
-//        var player: ExoPlayer? = ExoPlayer.Builder(requireContext())
-//            .setMediaSourceFactory(mediaSourceFactory)
-//            .build()
-
+        val btnMute = view?.findViewById<AppCompatImageView>(R.id.volume_control)
+       val btnFullScreen = view?.findViewById<AppCompatImageView>(R.id.fullscreen)
 
         player = context?.let {
-
-
             val trackSelector = DefaultTrackSelector(it).apply {
                 setParameters(buildUponParameters().setMaxVideoSizeSd())
             }
@@ -78,8 +69,6 @@ class HomeFragment : Fragment() {
                 .build()
                 .also { exoPlayer ->
                     binding.videoView.player = exoPlayer
-
-
                     val mediaItem =
                         MediaItem.fromUri("https://storage.googleapis.com/exoplayer-test-media-0/play.mp3")
                     exoPlayer.setMediaItem(mediaItem)
@@ -95,49 +84,34 @@ class HomeFragment : Fragment() {
 
                     btnMute?.setOnClickListener {
                         volumeInfo = if (volumeInfo) {
-                            btnMute.setImageResource(R.drawable.ic_volume_up)
-                            exoPlayer.volume = 1F
-                            !volumeInfo
-                        } else {
                             btnMute.setImageResource(R.drawable.ic_volume_off)
                             exoPlayer.volume = 0F
                             !volumeInfo
-                        }
-                    }
-                    btnPlay?.setOnClickListener {
-                        play = if (play) {
-                            btnPlay.setImageResource(R.drawable.ic_play_circle)
-                            exoPlayer.pause()
-                            currentPosition = exoPlayer.currentPosition
-                            !play
                         } else {
-                            btnPlay.setImageResource(R.drawable.ic_pause_circle)
-                            exoPlayer.play()
-                            exoPlayer.seekTo(currentPosition)
-                            !play
+                            btnMute.setImageResource(R.drawable.ic_volume_up)
+                            exoPlayer.volume = 1F
+                            !volumeInfo
                         }
                     }
-                    btnNext?.setOnClickListener {
-                            exoPlayer.seekToNextMediaItem()
-                    }
-                    btnPrevious?.setOnClickListener {
-                            exoPlayer.seekToPreviousMediaItem()
-                    }
-                    btnForward?.setOnClickListener {
-                       val position =  exoPlayer.currentPosition
-                        exoPlayer.seekTo(position+10000)
-                    }
-                    btnRewind?.setOnClickListener {
-                        val position =  exoPlayer.currentPosition
-                        exoPlayer.seekTo(position-10000)
-                    }
+                    btnFullScreen?.setOnClickListener {
+                        fullScreen = if (fullScreen) {
+                            btnFullScreen.setImageResource(R.drawable.ic_fullscreen_exit)
+                            hideSystemUi()
+                            binding.videoView.layoutParams = ConstraintLayout.LayoutParams(matchParent,matchParent)
+                            currentPosition = exoPlayer.currentPosition
+                            !fullScreen
+                        } else {
 
-
-
+                           showSystemUi()
+                            val density = context?.resources?.displayMetrics?.density
+                            binding.videoView.layoutParams = ConstraintLayout.LayoutParams(matchParent,(200* density!!).toInt())
+                            btnFullScreen.setImageResource(R.drawable.ic_fullscreen)
+                            !fullScreen
+                        }
+                    }
                 }
         }
     }
-
 
     private fun releasePlayer() {
         player?.let { exoPlayer ->
@@ -158,7 +132,7 @@ class HomeFragment : Fragment() {
 
     public override fun onResume() {
         super.onResume()
-        hideSystemUi()
+
         if ((Util.SDK_INT <= 23 || player == null)) {
             initializePlayer()
         }
@@ -184,6 +158,18 @@ class HomeFragment : Fragment() {
         activity?.let {
             WindowInsetsControllerCompat(it.window, binding.videoView).let { controller ->
                 controller.hide(WindowInsetsCompat.Type.systemBars())
+                controller.systemBarsBehavior =
+                    WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun showSystemUi() {
+        activity?.let { WindowCompat.setDecorFitsSystemWindows(it.window, true) }
+        activity?.let {
+            WindowInsetsControllerCompat(it.window, binding.videoView).let { controller ->
+                controller.show(WindowInsetsCompat.Type.systemBars())
                 controller.systemBarsBehavior =
                     WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
             }
