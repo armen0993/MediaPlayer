af4995ba08b31033c14d91fbe239f50c845d58c1
Armen <armenbeglaryan993@gmail.com>
2022-05-24 18:26:03 +0400

    changed exoplayer

--- app/src/main/java/com/example/mediaplayer/presentation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mediaplayer/presentation/ui/home/HomeFragment.kt
@@ -2,12 +2,17 @@ package com.example.mediaplayer.presentation.ui.home
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
+import android.content.res.Configuration.ORIENTATION_LANDSCAPE
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import androidx.activity.OnBackPressedDispatcher
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.WindowCompat
@@ -16,10 +21,14 @@ import androidx.core.view.WindowInsetsControllerCompat
 import androidx.fragment.app.Fragment
 import androidx.media3.common.MediaItem
 import androidx.media3.common.util.Util
+import androidx.media3.datasource.DefaultDataSourceFactory
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.trackselection.AdaptiveTrackSelection
 import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
+import androidx.media3.exoplayer.upstream.DefaultBandwidthMeter
 import com.example.mediaplayer.R
 import com.example.mediaplayer.databinding.FragmentHomeBinding
+import java.io.File
 
 
 class HomeFragment : Fragment() {
@@ -41,24 +50,24 @@ class HomeFragment : Fragment() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putLong("position",playbackPosition)
+        outState.putLong("position", playbackPosition)
     }
 
     override fun onViewStateRestored(savedInstanceState: Bundle?) {
         super.onViewStateRestored(savedInstanceState)
-       if (savedInstanceState?.get("position")!=null) {
-           playbackPosition = savedInstanceState.get("position") as Long
-       }
+        if (savedInstanceState?.get("position") != null) {
+            playbackPosition = savedInstanceState.get("position") as Long
+        }
     }
 
     private fun initializePlayer() {
         var volumeInfo = true
         var fullScreen = true
         var currentPosition = 0L
-       val matchParent =  ConstraintLayout.LayoutParams.MATCH_PARENT
+        val matchParent = ConstraintLayout.LayoutParams.MATCH_PARENT
 
         val btnMute = view?.findViewById<AppCompatImageView>(R.id.volume_control)
-       val btnFullScreen = view?.findViewById<AppCompatImageView>(R.id.fullscreen)
+        val btnFullScreen = view?.findViewById<AppCompatImageView>(R.id.fullscreen)
 
         player = context?.let {
             val trackSelector = DefaultTrackSelector(it).apply {
@@ -95,23 +104,48 @@ class HomeFragment : Fragment() {
                     }
                     btnFullScreen?.setOnClickListener {
                         fullScreen = if (fullScreen) {
+//                            val currentOrientation = activity?.requestedOrientation
+//                            if (currentOrientation != ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+//                                activity?.requestedOrientation =
+//                                    ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+//                            }
+
+                            binding.videoView.layoutParams =
+                                ConstraintLayout.LayoutParams(matchParent, matchParent)
                             btnFullScreen.setImageResource(R.drawable.ic_fullscreen_exit)
                             hideSystemUi()
-                            binding.videoView.layoutParams = ConstraintLayout.LayoutParams(matchParent,matchParent)
                             currentPosition = exoPlayer.currentPosition
                             !fullScreen
+
                         } else {
 
-                           showSystemUi()
+                            showSystemUi()
                             val density = context?.resources?.displayMetrics?.density
-                            binding.videoView.layoutParams = ConstraintLayout.LayoutParams(matchParent,(200* density!!).toInt())
+                            binding.videoView.layoutParams = ConstraintLayout.LayoutParams(
+                                matchParent,
+                                (200 * density!!).toInt()
+                            )
                             btnFullScreen.setImageResource(R.drawable.ic_fullscreen)
                             !fullScreen
                         }
                     }
                 }
         }
+//        if (!fullScreen){
+//            OnBackPressedDispatcher{
+//                showSystemUi()
+//                val density = context?.resources?.displayMetrics?.density
+//                binding.videoView.layoutParams = ConstraintLayout.LayoutParams(
+//                    matchParent,
+//                    (200 * density!!).toInt()
+//                )
+//                btnFullScreen?.setImageResource(R.drawable.ic_fullscreen)
+//                !fullScreen
+//            }
+//        }
+
     }
+
 
     private fun releasePlayer() {
         player?.let { exoPlayer ->
